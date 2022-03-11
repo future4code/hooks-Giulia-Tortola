f651069f549cb989dc48d7d49651ffdcde162cc0
Giulia-Verruck-Tortola <giuliaverruck@gmail.com>
2022-03-11 17:13:41 -0300

    Exercícios de Funções de Arrays - Finalizados

--- modulo1/funcoes-de-array/index.js
+++ modulo1/funcoes-de-array/index.js
@@ -4,9 +4,9 @@
 3. Imprimirá apenas os outros dois objetos que não tem "Chijo"*/
 
 
-/*Exercícios de Escrita de Código
+/*Exercícios de Escrita de Código*/
 
-*/
+
 const pets = [
     { nome: "Lupin", raca: "Salsicha"},
     { nome: "Polly", raca: "Lhasa Apso"},
@@ -15,19 +15,63 @@ const pets = [
     { nome: "Fluffy", raca: "Poodle"},
     { nome: "Caramelo", raca: "Vira-lata"},
  ]
-
- const apenasNomes = pets.map((item, index, array) => {
+// 1.a)
+ const apenasNomes = pets.map((item) => {
     return item.nome
  })
  
  console.log(apenasNomes)
-
- const apenasSalsicha = pets.filter((pets, index, array) => {
+//1.b)
+ const apenasSalsicha = pets.filter((pets) => {
     return pets.raca === "Salsicha"
  })
 
  console.log(apenasSalsicha)
+//1.c)
+const apenasPoddle = pets.filter ((pets) => pets.raca === "Poodle")
 
- const apenasPoddle = pets.filter ((pets,index,array)) => {
-     return pets.raca === "Poodle"
- } // acabou a luz!
+const promoPoddles = apenasPoddle.map((pet) => {
+   let aviso = (`Você ganhou 10% de desconto para o tosar o/a ${pet.nome}`)
+   return aviso
+})
+console.log(promoPoddles)
+//dados
+const produtos = [
+   { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+   { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+   { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+   { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+   { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+   { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+   { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+   { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+   { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+   { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+]
+//2.a) Crie um novo array que contenha apenas o nome de cada item
+
+const nomesProdutos = produtos.map((item) => {
+   return item.nome
+})
+console.log(nomesProdutos)
+
+//b) Crie um novo array que contenha um objeto com o nome e o preço de cada item, aplicando 5% de desconto em todos eles
+const descontoProdutos = produtos.map((item) => {
+    return{nome: item.nome, preco: ((item.preco)*0.95).toFixed(2)} 
+})
+console.log (descontoProdutos)
+//c) Crie um novo array que contenha apenas os objetos da categoria Bebidas
+const bebidas =  produtos.filter((item) => {
+   if (item.categoria === "Bebidas")
+   return item
+})
+console.log (bebidas)
+// d) Crie um novo array que contenha apenas os objetos cujo nome contenha a palavra "Ypê"
+const produtosYpe = produtos.filter(item =>item.nome.includes("Ypê"))
+console.log (produtosYpe)
+//e) Crie um novo array onde cada item é uma frase "Compre [NOME] por [PREÇO]". Esse array deve conter frases apenas dos itens cujo nome contenha a palavra "Ypê"
+const anuncioYpe = produtosYpe.map((produtosYpe) => {
+   let anuncio = (`Compre ${produtosYpe.nome} por ${produtosYpe.preco}`)
+   return anuncio
+})
+console.log(anuncioYpe)
