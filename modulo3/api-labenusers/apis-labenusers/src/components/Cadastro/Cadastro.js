f3ca7c0695fad506bfdbb416f463225cc3c2b0bb
Giulia-Verruck-Tortola <giuliaverruck@gmail.com>
2022-05-24 21:00:19 -0300

    Com a opção Delete integrada

--- modulo3/api-labenusers/apis-labenusers/src/components/Cadastro/Cadastro.js
+++ modulo3/api-labenusers/apis-labenusers/src/components/Cadastro/Cadastro.js
@@ -1,53 +1,62 @@
 import React from "react";
 import axios from "axios";
 
-
-const urlLbnApi = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users"
-
-const headers = {
-  headers: {
-    Authorization:'giulia-verruck-hooks'}
-  }
-
 class Cadastro extends React.Component{
   state = {
     newUsers: [],
-    inputNameUser: "",
-    inputMailUser:"",
-  }
-
-  createUser = () => {
-    const body = {
-      name: this.state.inputNameUser,
-      email: this.state.inputMailUser,
-    }
-    axios.post(urlLbnApi, body, headers)
-    .then((res) => {
-      alert(`Usuario ${this.state.inputNameUser} criado com sucesso!`)
-      this.setState({inputNameUser: ""})
-      this.setState({inputMailUser: ""})
-    })
-    .catch((err)=>{
-      console.log("Nada foi criado!")
-      this.setState({inputNameUser: ""})
-      this.setState({inputMailUser: ""})
-    })
+    nome: "",
+    email:"",
   }
 
   onCreateUser = (e) => {
-    this.setState({inputNameUser: e.target.value})
+    this.setState({nome: e.target.value})
   }
   onCreateUserMail = (e) => {
-    this.setState({inputMailUser: e.target.value})
+    this.setState({email: e.target.value})
   }
+  createUser = () => {
+    const urlLbnApi = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users"
+    const body = {
+      name: this.state.nome,
+      email: this.state.email,
+    }
+    const headers = {
+      headers: {
+      Authorization: "giulia-verruck-hooks"
+      }
+    }
+
+    axios.post(urlLbnApi, body, headers)
+    .then((response) => {
+      alert(`Usuario ${this.state.nome} criado com sucesso!`)
+      this.setState({nome: "", email: ""})
+     
+    })
+    .catch((error)=>{
+      console.log("Nada foi criado!")
+      this.setState({nome: "", email: ""})
+     
+    })
+  }
+
+  
   render(){
     return(
     <div>
+      <button onClick={() => {this.props.goBack("lista")}}>ir para Lista</button>
         <h1>Cadastro</h1>
-      <input onChange={this.onCreateUser} value={this.state.inputNameUser} placeholder="Nome"/>
-      <input onChange={this.onCreateUserMail} value={this.state.inputMailUser} placeholder="E-mail"/>
+      <input 
+        onChange={this.onCreateUser} 
+        value={this.state.nome} 
+        placeholder="Nome"
+      />
+      <input 
+        onChange={this.onCreateUserMail} 
+        value={this.state.email} 
+        placeholder="E-mail"
+      />
       <button onClick={this.createUser}>Cadastrar</button>
-      <button onClick={() => {this.props.goBack("lista")}}>Lista</button>
+      
       </div>
 
     )
--- modulo3/api-labenusers/apis-labenusers/src/components/Lista/Lista.js
+++ modulo3/api-labenusers/apis-labenusers/src/components/Lista/Lista.js
@@ -1,30 +1,65 @@
 import React from "react";
 import axios from "axios";
 
-const urlUsers = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users"
-
-const headers = {
-    headers: {
-    Authorization: "giulia-verruck-hooks"}
-}
-const getAllUsers = () => {
-    axios
-    .get (urlUsers, headers)
-    .then((response)=> {
-        this.setState({name: response.data.list})
-    console.log (response.data)})
-    .catch((error)=> {
-    console.log(error)})
-}
-
-getAllUsers()
 class Lista extends React.Component{
-   render(){
+  state= {
+    newUsers: []
+  }
+  componentDidMount () {
+    this.getAllUsers()
+  }
+ getAllUsers = () => {
+  
+    const urlUsers = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users"
+    
+    const headers = {
+        headers: {
+          Authorization: "giulia-verruck-hooks"}
+    }
+        axios
+        .get (urlUsers, headers)
+        .then((response)=> {
+          this.setState({newUsers: response.data})
+          console.log (response.data)})
+        .catch((error)=> {
+          console.log(error)})
+    }
+
+  deleteUser = (id) => {
+     
+    const url = `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${id}`
+    
+    const headers = {
+        headers: {
+          Authorization: "giulia-verruck-hooks"}
+  }
+  axios.delete(url, headers)
+    .then((response)=> {
+     alert("O Usuário foi deletado!")
+     this.getAllUsers()
+    })
+    .catch((error) => {
+      alert(error.response.data.message)
+    })
+}
+    
+   
+   render() {
+    const usuarios = this.state.newUsers.map((usuario) => {
+      return (
+        <div key={usuario.id}>
+          {usuario.name}
+          <button onClick={ () => this.deleteUser(usuario.id)}>X</button>
+        </div> 
+      )
+    })
+
     return(
-    <div>
-      <h1> Lista</h1>
-      <button onClick={()=>{this.props.goBack("cadastro")}}>Cadastro</button>
-    </div>
+      <div>
+        <button onClick={()=>{this.props.goBack("cadastro")}}>ir para Cadastro</button>
+        <h1> Lista</h1>
+        {usuarios}
+      </div>
 
     )
   }
