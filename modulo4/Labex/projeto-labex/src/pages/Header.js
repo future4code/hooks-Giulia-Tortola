73be5e1327420534eed95bf3663d203ec3a94a28
Giulia-Verruck-Tortola <giuliaverruck@gmail.com>
2022-06-14 18:37:50 -0300

    Pages

---
+++ modulo4/Labex/projeto-labex/src/pages/Header.js
@@ -0,0 +1,16 @@
+import styled  from "styled-components"
+
+const HeaderStyled = styled.header`
+width: 100%;
+height: 20px;
+background-color: blue ;
+`
+
+const Header = () => {
+    return 
+     <HeaderStyled>
+        <h1>LABEX</h1>
+     </HeaderStyled>
+}
+
+export default Header;
